74f4d16b18d35ffd31dc0795b4882c4771de5b22
D Jhansi <111398658+JhansiDhanabal@users.noreply.github.com>
2024-06-15 07:25:46 +0530

    Delete main directory

--- main/java/com/application/ecommerce_based_application/model/ValidationGroups.java
+++
@@ -1,6 +0,0 @@
-package com.application.ecommerce_based_application.model;
-
-public interface ValidationGroups {
-	interface DefaultGroup {}
-    interface OAuth2Group {}
-}
--- main/java/com/application/ecommerce_based_application/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.ecommerce_based_application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.application.ecommerce_based_application.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
-
-}
